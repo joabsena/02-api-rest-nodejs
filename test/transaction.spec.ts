9adc6719e3d4ac8d5b9c29efa2b39bc0c3d77ee3
joabsena <joabarbosa1@hotmail.com>
2023-02-26 16:39:32 -0300

    feat: added testing get specific transaction

--- test/transaction.spec.ts
+++ test/transaction.spec.ts
@@ -52,4 +52,36 @@ describe('Transactions routes', () => {
       }),
     ])
   })
+
+  it('should be able to get specific transaction', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201)
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    const transactionId = listTransactionResponse.body.transactions[0].id
+
+    const getTransactionResponse = await request(app.server)
+      .get(`/transactions/${transactionId}`)
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(getTransactionResponse.body.transaction).toEqual(
+      expect.objectContaining({
+        title: 'New Transaction',
+        amount: 5000,
+      }),
+    )
+  })
 })
